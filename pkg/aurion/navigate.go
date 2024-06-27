b038571ddbec67d569fbe2b00ab47bad37f10d00
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-06-27 10:52:12 +0200

    feat: dynamically loading submenu and choosing sidebar ID

--- pkg/aurion/navigate.go
+++ pkg/aurion/navigate.go
@@ -1,17 +1,19 @@
 package aurion
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-type MenuId string
+type MenuId []string
 
 // MenuNavigateTo
-func MenuNavigateTo(token Token, menu_id MenuId, mainMenuUrl string) ([]byte, error) {
+func MenuNavigateTo(token Token, menuId MenuId, mainMenuUrl string) ([]byte, error) {
 	// Set client
 	client := &http.Client{}
 
@@ -35,8 +37,20 @@ func MenuNavigateTo(token Token, menu_id MenuId, mainMenuUrl string) ([]byte, er
 		return nil, fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
 
+	// clone body for two different functions
+	bodyBuf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	body1 := io.NopCloser(bytes.NewBuffer(bodyBuf))
+	body2 := io.NopCloser(bytes.NewBuffer(bodyBuf))
 	// Get viewstate to request destination page
-	viewState, err := getViewState(resp.Body)
+	viewState, err := getViewState(body1)
+	if err != nil {
+		return nil, err
+	}
+
+	sidebarId, err := getSidebarId(token, viewState, body2, menuId, mainMenuUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -44,7 +58,7 @@ func MenuNavigateTo(token Token, menu_id MenuId, mainMenuUrl string) ([]byte, er
 	// Prepare post on homepage to be redirect on good page
 	formData := url.Values{
 		"javax.faces.ViewState": {string(viewState)},
-		"form:sidebar_menuid":   {string(menu_id)},
+		"form:sidebar_menuid":   {string(sidebarId)},
 		"form:sidebar":          {"form:sidebar"},
 		"form":                  {"form"},
 	}
@@ -71,21 +85,3 @@ func MenuNavigateTo(token Token, menu_id MenuId, mainMenuUrl string) ([]byte, er
 
 	return responseBody, nil
 }
-
-// func GetMenuId(pageContent []byte, menuPath string) (string, error) {
-// 	// Get only #form:sidebar
-// 	reader := bytes.NewReader(pageContent)
-// 	doc, err := goquery.NewDocumentFromReader(reader)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	doc.Find("#form:sidebar>ui-menu-list").Each(func(i int, s *goquery.Selection) {
-// 		fmt.Printf("%s", s.Text())
-// 		s.Find("ul[role='gridcell']").Each(func(i int, s *goquery.Selection) {
-
-// 		})
-// 	})
-
-// 	return "", nil
-// }
